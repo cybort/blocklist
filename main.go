cd73ed5a1c217d1224d85f7b30c99810964cc2c8
Fan Yang <missdeer@dfordsoft.com>
2017-02-19 23:00:25 +0800

    (+)add a go script to make the final list WIP

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	// download hosts
+	// extract domain names
+	// remove items that don't match xxxx.xxxx.xxxx format
+	// remove items that don't match TLDs
+	// remove duplicate items
+	// remove items in white list
+}
