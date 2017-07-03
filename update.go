d4a4c825c9fe96c0aced3073c87883c7a33016b1
Fan Yang <me@minidump.info>
2017-07-03 11:24:26 +0800

    fixed #13

--- update.go
+++ update.go
@@ -56,6 +56,7 @@ var (
 		regex(`^[^\.]+\.elb\.amazonaws\.com`),
 		suffix(`.in-addr.arpa`),
 		equal(`analytics.163.com`),
+		equal(`behance.net`),
 	}
 	tlds               = make(map[string]struct{})
 	tldsMutex          sync.Mutex
